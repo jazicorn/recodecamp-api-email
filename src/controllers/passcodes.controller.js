1b8db875cd6e9fcb2a2cdfef904a0f65913d5d32
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-11-01 04:22:20 -0400

    update data const

--- src/controllers/passcodes.controller.js
+++ src/controllers/passcodes.controller.js
@@ -15,11 +15,12 @@ require('dotenv').config();
 const guestConfirmAccount = async (req, res) => {
     switch (req.method) {
         case 'POST':
-            const data = req.body;
+            const data = req.body.data;
             //console.log("data", data);
             const email = data.email;
             const passcode = data.passcode;
-            const url = `${process.env.WEBURL}/auth/account/confirm/${passcode}`;
+            const url = `${process.env.WEB_URL_ACCOUNT_CONFIRM}/${passcode}`;
+            //console.log("url:", url)
 
             // Mail options
             const mailOptions = {
